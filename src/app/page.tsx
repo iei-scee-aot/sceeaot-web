d9d453b2bab64d2f5c6109c35f813a50c06bb583
debarshee2004 <debarsheechakraborty.11d@gamil.com>
2024-10-02 17:28:58 +0530

    Added Image Slider to the Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-export default function Home() {
+import React from "react";
+import ImageSlider from "@/components/layout/ImageSlider";
+
+const Apps = () => {
+  const slides = [
+    {
+      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869068/imageslider-1_g9lkde.jpg",
+      title: "Test",
+    },
+    {
+      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869386/imageslider-2_jyhrtt.jpg",
+      title: "Test",
+    },
+    {
+      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869068/imageslider-1_g9lkde.jpg",
+      title: "Test",
+    },
+    {
+      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869386/imageslider-2_jyhrtt.jpg",
+      title: "Test",
+    },
+    {
+      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869068/imageslider-1_g9lkde.jpg",
+      title: "Test",
+    },
+  ];
   return (
     <main className="pt-24">
       <div>
-        <h1>Hello Home Page</h1>
+        <ImageSlider slides={slides} />
       </div>
     </main>
   );
-}
+};
+
+export default Apps;
---
+++ src/components/layout/ImageSlider.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { useState } from "react";
+
+interface Slide {
+  url: string;
+}
+
+interface ImageSliderProps {
+  slides: Slide[];
+}
+
+const ImageSlider: React.FC<ImageSliderProps> = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div className="relative h-[600px] pr-20 pl-20">
+      {/* Left Arrow */}
+      <div
+        className="absolute top-1/2 left-8 transform -translate-y-1/2 text-4xl text-white z-10 cursor-pointer"
+        onClick={goToPrevious}
+      >
+        ❰
+      </div>
+
+      {/* Right Arrow */}
+      <div
+        className="absolute top-1/2 right-8 transform -translate-y-1/2 text-4xl text-white z-10 cursor-pointer"
+        onClick={goToNext}
+      >
+        ❱
+      </div>
+
+      {/* Slide */}
+      <div
+        className="w-full h-full rounded-lg bg-cover bg-center"
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+      ></div>
+
+      {/* Dots */}
+      <div className="flex justify-center mt-4">
+        {slides.map((_, slideIndex) => (
+          <div
+            key={slideIndex}
+            className={`mx-1 cursor-pointer text-2xl ${
+              slideIndex === currentIndex ? "text-yellow-400" : "text-gray-400"
+            }`}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            ●
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
