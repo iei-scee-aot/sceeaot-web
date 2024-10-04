5980ffaec2e34a7f440b6fd92215085b92098d23
debarshee2004 <debarsheechakraborty.11d@gamil.com>
2024-10-04 11:07:29 +0530

    Optimised Syntax

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
-
 import ticket from "@/assets/icons/ticket-icon.png";
 
 const Events = () => {
