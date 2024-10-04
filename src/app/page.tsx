f528a56bba1ecdc0fd653678971115ab010c2cf7
debarshee2004 <debarsheechakraborty.11d@gamil.com>
2024-10-04 07:45:08 +0530

    Updated Image Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,32 @@
 import React from "react";
-import ImageSlider from "@/components/layout/ImageSlider";
+import ImageSlider from "@/app/components/ImageSlider";
 import { Button } from "@/components/ui/button";
 
 const Apps = () => {
   const slides = [
     {
-      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869068/imageslider-1_g9lkde.jpg",
+      url: "https://utfs.io/f/kQP3nlH8TqeSpHFgKicRM3Egt9wzXbuhLHZOkBT5iFU4r6DP",
       title: "Test",
     },
     {
-      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869386/imageslider-2_jyhrtt.jpg",
+      url: "https://utfs.io/f/kQP3nlH8TqeS3DUgzv2rPkapnMo4e8jVXyDIZK2rqwxCH1mW",
       title: "Test",
     },
     {
-      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869068/imageslider-1_g9lkde.jpg",
-      title: "Test",
-    },
-    {
-      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869386/imageslider-2_jyhrtt.jpg",
-      title: "Test",
-    },
-    {
-      url: "https://res.cloudinary.com/desqmyb7h/image/upload/v1727869068/imageslider-1_g9lkde.jpg",
+      url: "https://utfs.io/f/kQP3nlH8TqeSyTRxB9nokeRMbzdKJwA1aWGhcCPmIOt7jTgi",
       title: "Test",
     },
   ];
   return (
     <main className="pt-24">
-      <div className="w-full flex justify-center items-center p-10">
-        <h1 className="text-4xl font-bold text-yellow-400">
-          Home to the COOLEST Students&#x2019; Chapter of all Time
-        </h1>
-      </div>
       <div>
         <ImageSlider slides={slides} />
       </div>
       <div className="w-full flex justify-between items-center">
         <div className="p-20 w-[1200px]">
+          <h1 className="text-4xl font-bold text-yellow-400">
+            Home to the COOLEST Students&#x2019; Chapter of all Time
+          </h1>
           <p className="text-sm">
             Welcome to the IEI Students&#x2019; Chapter of EE AOT! We&#x2019;re
             all about innovation, tech vibes, and leveling up the skills. Join
@@ -48,6 +38,9 @@ const Apps = () => {
           <Button className="mr-5">Join Us</Button>
           <Button variant="outline">Explore Events</Button>
         </div>
+      </div>
+      <div>
+        <div className="ml-20 mr-20 border border-gray-300 border-dashed"></div>
       </div>
     </main>
   );
