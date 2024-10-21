ddb72342c7a04b8e1c797d5db58f254aa4ebb73d
Debpratim Sharma <debpratimsharma33@gmail.com>
2024-10-21 16:28:06 +0530

    footer fixed

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,38 +8,44 @@ const Footer = () => {
   return (
     <footer className="rounded-lg shadow m-4">
       <div className="w-full max-w-screen mx-auto pl-16 pr-16 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="sm:flex flex-col md:flex-row  sm:items-center sm:justify-between">
           <Link
             href="/"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            className="flex flex-wrap justify-center items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <Image src={sceelogo} alt="SCEEAOT" width={100} className="h-8" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-              Students Chapter of Electrical Engineering
-            </span>
+            <div><Image src={sceelogo} alt="SCEEAOT" width={100} className="h-8" /></div>
+            <div className="text-center">
+              <span className="self-center md:text-2xl text-sm font-semibold mr-10 text-white">
+                Students Chapter of Electrical Engineering
+              </span>
+            </div>
           </Link>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
+          <div>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  Licensing
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          
         </div>
         <hr className="my-6 border-gray-200 border-dashed sm:mx-auto lg:my-8" />
         <span className="block text-sm text-yellow-300 sm:text-center">
