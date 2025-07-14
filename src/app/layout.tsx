9e7a7e6e00a7d44e0b28fe237e40fcef9b8be66a
arnab-15june2003 <arnab.biswas.rk108@gmail.com>
2025-07-14 19:53:25 +0530

    added scee icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/scee_icon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
