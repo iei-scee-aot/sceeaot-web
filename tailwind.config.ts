8a0faa714d484fc6a0048a4f313979426d7bc9e9
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-07-24 11:19:14 +0530

    Team page layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,14 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.json",
   ],
   theme: {
     extend: {
       keyframes: {
         marquee: {
-          '0%': { transform: "translateX(0)" },
-          '100%': { transform: "translateX(-50%)" },
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
         },
       },
       animation: {
