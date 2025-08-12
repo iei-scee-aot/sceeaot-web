eadf4f753a931ecbfcc9fd9d8bb7e092deab426e
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-08-12 19:22:22 +0530

    Updated routes

--- src/app/iotricity/page.tsx
+++ src/app/iotricity/page.tsx
@@ -216,7 +216,7 @@ const IOTricityPage = () => {
         </div>
 
         <div className="flex flex-col sm:flex-row gap-4 mt-8 mb-12 px-4 justify-center items-center">
-          <Link href="/problems" className="w-full sm:w-auto">
+          <Link href="/iotricity/problems" className="w-full sm:w-auto">
             <Button
               variant="secondary"
               icon={<ExternalLink size={18} />}
@@ -225,7 +225,7 @@ const IOTricityPage = () => {
               Problem Statements
             </Button>
           </Link>
-          <Link href="/rules" target="_blank" className="w-full sm:w-auto">
+          <Link href="/iotricity/rules" className="w-full sm:w-auto">
             <Button
               variant="secondary"
               icon={<ExternalLink size={18} />}
