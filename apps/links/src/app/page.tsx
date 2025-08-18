c089912491a662a2b5a484d1d3f968da89ab2e65
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-08-18 18:25:38 +0530

    Updated links page

--- apps/links/src/app/page.tsx
+++ apps/links/src/app/page.tsx
@@ -4,20 +4,49 @@ import {
   LinksFooter,
   LinksGrid,
   LinksHeader,
-  linksData,
+  socialMediaLinks,
+  websiteLinks,
 } from "@/components/links";
+import Divider from "@/components/ui/Divider";
+import Headlines from "@/components/ui/Headlines";
 
 const LinksPage = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <div>
-        {/* Header */}
-        <div className="w-[calc(100%-30px)] lg:w-[calc(100%-14rem)] mx-auto">
-          <div className="px-6 lg:px-12 py-8 lg:py-16">
-            <LinksHeader />
-            <LinksGrid links={linksData} />
-            <LinksFooter />
-          </div>
+      {/* Header Section */}
+      <div className="w-[calc(100%-30px)] lg:w-[calc(100%-14rem)] mx-auto border-[0.5px] border-gray-500 border-t-0 border-b-0">
+        <div className="px-6 lg:px-12 py-8 lg:py-16">
+          <LinksHeader />
+        </div>
+      </div>
+      <Divider />
+
+      {/* Website Links Section */}
+      <div className="w-[calc(100%-30px)] lg:w-[calc(100%-14rem)] mx-auto border-[0.5px] border-gray-500 border-t-0 border-b-0">
+        <div className="flex items-center border-gray-500 border-b-[0.5px] overflow-hidden">
+          <Headlines headLine="Website Links" />
+        </div>
+        <div className="px-6 lg:px-12 py-8 lg:py-16">
+          <LinksGrid links={websiteLinks} />
+        </div>
+      </div>
+      <Divider />
+
+      {/* Social Media Links Section */}
+      <div className="w-[calc(100%-30px)] lg:w-[calc(100%-14rem)] mx-auto border-[0.5px] border-gray-500 border-t-0 border-b-0">
+        <div className="flex items-center border-gray-500 border-b-[0.5px] overflow-hidden">
+          <Headlines headLine="Social Media" />
+        </div>
+        <div className="px-6 lg:px-12 py-8 lg:py-16">
+          <LinksGrid links={socialMediaLinks} />
+        </div>
+      </div>
+      <Divider />
+
+      {/* Footer Section */}
+      <div className="w-[calc(100%-30px)] lg:w-[calc(100%-14rem)] mx-auto border-[0.5px] border-gray-500 border-t-0 border-b-0">
+        <div className="px-6 lg:px-12 py-8 lg:py-16">
+          <LinksFooter />
         </div>
       </div>
     </div>
