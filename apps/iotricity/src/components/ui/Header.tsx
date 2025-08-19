5c1ea4b8f4204b51d55ba2813b88272b41aef717
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-08-19 20:08:51 +0530

    Added Discord Links

--- apps/iotricity/src/components/ui/Header.tsx
+++ apps/iotricity/src/components/ui/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
       </div>
       <div className=" flex items-center justify-center">
         <a
-          href="https://discord.gg/mqNcynTs"
+          href="https://discord.gg/K6GfRPAS"
           target="_blank"
           rel="noopener noreferrer"
         >
