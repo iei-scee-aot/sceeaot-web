2df6b13b8c70142e903bb13d77b9166ad43230ad
Achinta <achintasiddhanta0304@gmail.com>
2026-03-25 22:21:38 +0530

    dont need the hasTargetPassed

--- apps/iotricity/src/components/iotricity/PrizePool.tsx
+++ apps/iotricity/src/components/iotricity/PrizePool.tsx
@@ -7,10 +7,6 @@ import { ExternalLink } from 'lucide-react'
 
 const PrizePool = () => {
 
-  const targetedDate = new Date(event.registrationEnd).getTime();
-  const now = new Date().getTime();
-  const hasTargetDatePassed = now >= targetedDate;
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full mb-6">
         {prizePool.map(val => (
@@ -18,8 +14,7 @@ const PrizePool = () => {
         ))}
 
         <div className="col-span-full flex justify-center mt-2">
-            {!hasTargetDatePassed ? (
-                <Link href={event.registrationLink} target="_blank">
+            <Link href={event.registrationLink} target="_blank">
                 <Button
                     variant="secondary"
                     className="group border-primary/20 hover:border-primary/50"
@@ -33,19 +28,6 @@ const PrizePool = () => {
                     </span>
                 </Button>
             </Link>
-            ) : (
-                <div className="cursor-not-allowed w-full sm:w-auto">
-                    <Button
-                        variant="secondary"
-                        className="opacity-50 pointer-events-none w-full" 
-                        disabled={true}
-                    >
-                        <span className="relative z-10 flex items-center gap-2">
-                        Registration Closed
-                        </span>
-                    </Button>
-                </div>
-            )}
         </div>
     </div>
   )
