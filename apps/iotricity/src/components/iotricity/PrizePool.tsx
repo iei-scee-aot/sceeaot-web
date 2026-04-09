33d493139aeebbcd0a54fa9f270d52b547d2abfe
Achinta <achintasiddhanta0304@gmail.com>
2026-04-09 21:32:13 +0530

    fix#: a timezone bug and updated registrationend date

--- apps/iotricity/src/components/iotricity/PrizePool.tsx
+++ apps/iotricity/src/components/iotricity/PrizePool.tsx
@@ -3,9 +3,15 @@ import { event, prizePool } from '../../../constants'
 import PrizePoolCard from './PrizePoolCard'
 import Link from 'next/link'
 import Button from '../ui/Button'
-import { ExternalLink } from 'lucide-react'
+import { ArrowRight, ExternalLink } from 'lucide-react'
 
 const PrizePool = () => {
+
+  const { registrationEnd, registrationLink } = event;
+
+  const targetedDate = new Date(registrationEnd).getTime();
+  const now = new Date().getTime();
+  const hasTargetDatePassed = now >= targetedDate;
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full mb-6">
@@ -14,20 +20,35 @@ const PrizePool = () => {
         ))}
 
         <div className="col-span-full flex justify-center mt-2">
-            <Link href={event.registrationLink} target="_blank">
-                <Button
-                    variant="secondary"
-                    className="group border-primary/20 hover:border-primary/50 hover:translate-y-1 hover:scale-95 transititon-transform duration-300"
-                >
-                    <span className="flex items-center gap-2">
-                    Register Now
-                    <ExternalLink
-                        size={16}
-                        className="transition-transform group-hover:scale-110"
-                    />
-                    </span>
-                </Button>
-            </Link>
+            {hasTargetDatePassed ? (
+                  <div className="cursor-not-allowed w-full sm:w-auto">
+                    <Button
+                      variant="primary"
+                      className="opacity-50 pointer-events-none w-full hover:translate-y-1 hover:scale-95 transititon-transform duration-300" 
+                      disabled={true}
+                    >
+                      <span className="relative z-10 flex items-center gap-2">
+                        Registration Closed
+                      </span>
+                    </Button>
+                  </div>
+                ) : (
+                  <Link href={registrationLink} target='_blank' className="w-full sm:w-auto">
+                    <Button
+                      variant="primary"
+                      className="group relative overflow-hidden w-full hover:translate-y-1 hover:scale-95 transition-transform duration-300"
+                    >
+                      <span className="relative z-10 flex items-center gap-2">
+                        Register Now
+                        <ArrowRight
+                          size={18}
+                          className="transition-transform group-hover:translate-x-1"
+                        />
+                      </span>
+                      <div className="absolute inset-0 bg-gradient-to-r from-primary/80 to-yellow-400/80 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                    </Button>
+                  </Link>
+                )}
         </div>
     </div>
   )
